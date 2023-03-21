d71a0e222e02effe2afc275429eacac56aa6b8d8
jatinkh25 <jatinkhatri25.jk@gmail.com>
2023-03-21 23:19:25 +0530

    state persistance between refreshes added and view game bug resolved

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -3,3 +3,15 @@ import { BigNumber } from 'ethers'
 export const convertHexadecimalToNumber = ({ _hex }: BigNumber) => {
   return parseInt(_hex)
 }
+
+export const getSessionStorageItem = (key: string) => {
+  return sessionStorage.getItem(key)
+}
+
+export const setSessionStorageItem = (key: string, value: string) => {
+  sessionStorage.setItem(key, value)
+}
+
+export const clearSessionStorage = () => {
+  sessionStorage.clear()
+}
